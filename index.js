9f9164a7680752796132c6733dd74d6fc88cf274
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-24 12:10:05 +0100

    creating project

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = process.env.MY_NAME;
+const userCity = process.env.MY_CITY;
+const userLanguage = process.env.MY_LANGUAGE;
+
+console.log(`I am ${userName}, wilder in ${userCity}, and I love ${userLanguage}.`);
